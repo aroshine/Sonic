f774126150d76c9b0fe87575041a574c1a9967eb
aroshine <aroshine@kth.se>
2016-12-05 00:20:55 +0100

    Update

--- nowplaying.js
+++ nowplaying.js
@@ -1,44 +1,65 @@
 $(document).ready(function(){
-	$.getJSON('today-top-hits.json', function (data) {
-		$('#subheader').html(data.playlistName);
+  $.getJSON('today-top-hits.json', function (data) {
+    $('#subheader').html(data.playlistName);
 
     var i = 0;
     console.log(i);
-		var eachtrack = data.tracks.map(function (track) {
+    var eachtrack = data.tracks.map(function (track) {
         return '<img src="' + track.artwork + '" alt="art" />' 
-        		+ '<div id="songtitle">' + track.title +'</div>' 
-        		+ '<div id="artist">' + track.artists + '</div>'
-        		+ '<audio id="songtrack" src="' + track.preview_url + '"/>';
-      	});
+            + '<div id="songtitle">' + track.title +'</div>' 
+            + '<div id="artist">' + track.artists + '</div>'
+            + '<audio id="songtrack" src="' + track.preview_url + '"/>';
+        });
+    
+    //play
     $('#play').on('click', function() {
-    document.getElementById('songtrack').play();
+      document.getElementById('songtrack').play();
+      $('#play').hide();
+      $('#pause').show();
+      showDuration();
     });
 
+    //pause
     $('#pause').on('click', function() {
-    document.getElementById('songtrack').pause();
+      document.getElementById('songtrack').pause();
+      $('#pause').hide();
+      $('#play').show();
     });
+
+    //initializer -play first song
+    $('#pause').hide();
     $('#eachtrack').html(eachtrack[0]);
+
+    //seekbar
+    function showDuration(){
     $('#songtrack').on('timeupdate', function() {
-    $('#seekbar').attr("value", this.currentTime / this.duration);
+      $('#seekbar').attr("value", this.currentTime / this.duration);
     });
+    }
+
+    //next
     var i = 0;
     var track_length = data.tracks.length;
-    console.log(track_length);
-
     $('#next').on('click', function(){
       i = i + 1; // increase i by one
       i = i % track_length // if we've gone too high, start from `0` again
-      console.log(i);
-      return $('#eachtrack').html(eachtrack[i]) 
-             + $('songtrack').html(songtrack[i]); // give us back the item of where we are now
-    });
+      return $('#eachtrack').html(eachtrack[i]) // give us back the item of where we are now
+             + document.getElementById('songtrack').play()
+             + showDuration()
+             + $('#play').hide()
+             + $('#pause').show();;
+    });  
+    //previous  
     $('#prev').on('click', function(){
       if (i === 0){
         i = track_length;
       }
       i = i - 1;
-      return $('#eachtrack').html(eachtrack[i]) 
-             + $('songtrack').html(songtrack[i]);  
+      return $('#eachtrack').html(eachtrack[i])
+             + document.getElementById('songtrack').play()
+             + showDuration()
+             + $('#play').hide()
+             + $('#pause').show();;           
     });
 
   });
